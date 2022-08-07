ed96bc2f322ddfc2c5ac16fd420e9ff9c82de8ac
Sunderhus <matheus.sunderhus@gmail.com>
2022-08-06 23:09:23 -0300

    feat(CloseButton):  component creation

---
+++ src/components/close-button/index.vue
@@ -0,0 +1,18 @@
+<script>
+import { PopoverButton } from "@headlessui/vue";
+export default {
+  name: "CloseButton",
+  components: {
+    PopoverButton,
+  },
+};
+</script>
+
+<template>
+  <PopoverButton
+    class="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100"
+    title="Fechar formulário"
+  >
+    <PhX weight="bold" class="w-4 h-4" />
+  </PopoverButton>
+</template>
