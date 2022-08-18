a1b6003b67186877b32178cd7658db05911f4c66
Sunderhus <matheus.sunderhus@gmail.com>
2022-08-18 19:12:26 -0300

    feat: expose widget component and create basic module federation configuration

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,24 @@
 const { defineConfig } = require("@vue/cli-service");
-const {container:{ModuleFederationPlugin}} = require('webpack')
+const { container: { ModuleFederationPlugin } } = require('webpack')
 
 module.exports = defineConfig({
-    publicPath:'http://localhost:8080/',
+    publicPath: 'auto',
     transpileDependencies: true,
-    configureWebpack:{
-    optimization:{
-        splitChunks:false,
-    },
-    plugins:[
-        new ModuleFederationPlugin({
-            name:'widgetFeedback',
-            filename:'remoteEntry.js',
-            exposes:{
-                './Widget':'./src/components/widget/index.vue',
-            },
-            remotes:{},
-            shared:{
-                ...require('./package.json').dependencies
-            },
-        })
-    ]
-  }
+    configureWebpack: {
+        optimization: {
+            splitChunks: false,
+        },
+        plugins: [
+            new ModuleFederationPlugin({
+                name: 'widgetFeedback',
+                filename: 'remoteEntry.js',
+                exposes: {
+                    './Widget': './src/components/widget/index.vue',
+                },
+                remotes: {},
+                shared: {
+                },
+            })
+        ]
+    }
 });
